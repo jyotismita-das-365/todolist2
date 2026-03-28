21917b092261927b5a0f0bda26b03368e091a65e
jyotismita-das-365 <jyotismitadas365@gmail.com>
2026-03-28 09:25:36 +0530

    Practice

--- backend/index.js
+++ backend/index.js
@@ -36,11 +36,50 @@ app.get("/tasks", async (req, resp) => {
   }
 });
 
+app.get("/task/:id", async (req, resp) => {
+  const db = await connection();
+  const collection = await db.collection(collectionName);
+  const id = req.params.id
+  const result = await collection.findOne({_id: new ObjectId(id)});
+  if (result) {
+    resp.send({ message: "task fetched", success: true, result });
+  } else {
+    resp.send({ message: "error try after sometime", success: false });
+  }
+});
+
+app.put("/update-task", async (req, resp) => {
+  const db = await connection();
+  const collection = await db.collection(collectionName);
+  const {_id, ...fields} = req.body;
+  const update = {$set:fields}
+  console.log(req.body)
+  const result = await collection.updateOne({_id:new ObjectId(_id)}, update);
+  if (result) {
+    resp.send({ message: "task data update", success: true, result });
+  } else {
+    resp.send({ message: "error try after sometime", success: false });
+  }
+});
+
 app.delete("/delete/:id", async (req, resp) => {
   const db = await connection();
   const id = req.params.id
   const collection = await db.collection(collectionName);
   const result = await collection.deleteOne({_id: new ObjectId(id)});
+  if (result) {
+    resp.send({ message: "task deleted", success: true, result });
+  } else {
+    resp.send({ message: "error try after sometime", success: false });
+  }
+});
+
+app.delete("/delete-multiple", async (req, resp) => {
+  const db = await connection();
+  const ids = req.body;
+  console.log(ids);
+  // const collection = await db.collection(collectionName);
+  // const result = await collection.deleteOne({_id: new ObjectId(id)});
   if (result) {
     resp.send({ message: "task deleted", success: true, result });
   } else {
